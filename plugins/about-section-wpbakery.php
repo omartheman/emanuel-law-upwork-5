c52aa512099b4b15b2222110307929e649f28596
Omar Shishani <omarshishanischool@gmail.com>
2026-01-25 11:10:38 -0800

    Create about-section-wpbakery.php

---
+++ plugins/about-section-wpbakery.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+ * Plugin Name: About Page Section
+ * Plugin URI: 
+ * Description: A WPBakery module for an "About" section
+ * Version 1.0
+ * Author: Omar Shishani
+ * Author URI: https://omarshishani.com
+ */
