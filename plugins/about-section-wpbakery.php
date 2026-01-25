a35c7a4aebca9fe2fd5116eb0f27bdf47860b9ae
Omar Shishani <omarshishanischool@gmail.com>
2026-01-25 11:16:17 -0800

    Add code to define shortcode

--- plugins/about-section-wpbakery.php
+++ plugins/about-section-wpbakery.php
@@ -7,3 +7,21 @@
  * Author: Omar Shishani
  * Author URI: https://omarshishani.com
  */
+
+if ( ! function_exists( 'wpb_dev_example_custom_shortcode_output' ) ) :
+function wpb_dev_example_custom_shortcode_output( $atts, $content = null ) {
+    extract( shortcode_atts( array(
+        'title' => 'Default Title',
+        'description' => 'Default description.',
+    ), $atts ) );
+
+    // Output the HTML for your module
+    $output = '<div class="custom-module">CUSTOM MODULE';
+    $output .= '<h3>' . esc_html($title) . '</h3>';
+    $output .= '<p>' . esc_html($description) . '</p>';
+    $output .= '</div>';
+
+    return $output;
+}
+endif;
+add_shortcode( 'wpb_dev_custom_module', 'wpb_dev_example_custom_shortcode_output' );
