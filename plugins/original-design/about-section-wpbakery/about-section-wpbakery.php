62d76b5c383b645d08cfb9babe48fdb3e6742e7d
Omar Shishani <omarshishanischool@gmail.com>
2026-02-06 14:13:58 -0800

    Move files; Duplicate plugin from last site

---
+++ plugins/original-design/about-section-wpbakery/about-section-wpbakery.php
@@ -0,0 +1,123 @@
+<?php 
+/**
+ * Plugin Name: About Page Section WPBakery
+ * Plugin URI: 
+ * Description: A WPBakery module for an "About" section
+ * Version: 1.1
+ * Author: Omar Shishani
+ * Author URI: https://omarshishani.com
+ */
+
+if ( ! function_exists( 'wpb_dev_example_custom_shortcode_output' ) ) :
+function wpb_dev_example_custom_shortcode_output( $atts, $content = null ) {
+
+    $atts = shortcode_atts( array(
+        'title'    => 'About',
+        'name'     => 'Emanuel Orlando',
+        'el_class' => '',
+        'css'      => '',
+    ), $atts );
+
+    $title    = $atts['title'];
+    $el_class = $atts['el_class'];
+    $name    = $atts['name'];
+
+    // WPBakery Design Options CSS
+    $css_class = vc_shortcode_custom_css_class( $atts['css'], ' ' );
+
+    $output = '
+    <div class="custom-module' . esc_attr( $css_class ) . ' ' . esc_attr( $el_class ) . '">
+        <h3 class="profile-title">' . esc_html( $title ) . '</h3>
+    </div>
+
+    <div class="profile-container' . esc_attr( $css_class ) . ' ' . esc_attr( $el_class ) . '">
+        <div class="portrait-container">
+            <img loading="lazy" decoding="async" 
+                 src="' . esc_url( "https://icolaw.net/wp-content/uploads/2026/01/Group-1.png" ) . '" 
+                 alt="Profile photo of Emanuel Orlando." 
+                 width="320" height="400">
+        </div>
+        <div class="profile-textarea">';
+
+    $output .= '<div class="profile-name">' . esc_html( $name ) . '</div>';
+
+    $output .= wpautop( wp_kses_post( $content ) ) . '
+        </div>
+    </div>
+    ';
+
+    return $output;
+}
+endif;
+
+add_shortcode( 'wpb_dev_custom_module', 'wpb_dev_example_custom_shortcode_output' );
+
+
+function wpb_dev_example_map_custom_module() {
+
+    if ( function_exists( 'vc_map' ) ) :
+        vc_map( array(
+            'name' => __( '"About" Module', 'text-domain' ),
+            'base' => 'wpb_dev_custom_module',
+            'category' => __( 'My Elements', 'text-domain' ),
+            'params' => array(
+
+                array(
+                    'type' => 'textfield',
+                    'holder' => 'h3',
+                    'heading' => __( 'Name', 'text-domain' ),
+                    'param_name' => 'name',
+                    'value' => __( 'Default Title', 'text-domain' ),
+                    'description' => __( 'Enter the module title.', 'text-domain' ),
+                ),
+
+                array(
+                    'type' => 'textfield',
+                    'holder' => 'h2',
+                    'heading' => __( 'Title', 'text-domain' ),
+                    'param_name' => 'title',
+                    'value' => __( 'Default Title', 'text-domain' ),
+                    'description' => __( 'Enter the module title.', 'text-domain' ),
+                ),
+
+                array(
+                    'type' => 'textarea_html',
+                    'heading' => __( 'Description', 'text-domain' ),
+                    'param_name' => 'content',
+                    'description' => __( 'Enter the module description. Line breaks will create paragraphs.', 'text-domain' ),
+                ),
+
+                // Extra class
+                array(
+                    'type' => 'textfield',
+                    'heading' => __( 'Extra class name', 'text-domain' ),
+                    'param_name' => 'el_class',
+                    'description' => __( 'Add a class name for custom styling.', 'text-domain' ),
+                ),
+
+                // Design Options tab
+                array(
+                    'type' => 'css_editor',
+                    'heading' => __( 'Design Options', 'text-domain' ),
+                    'param_name' => 'css',
+                    'group' => __( 'Design Options', 'text-domain' ),
+                ),
+            ),
+        ) );
+    endif;
+}
+
+add_action( 'vc_before_init', 'wpb_dev_example_map_custom_module' );
+
+
+function about_plugin_enqueue_styles() {
+    wp_enqueue_style(
+        'ico-about-section-styles',
+        plugins_url( '/css/style.css', __FILE__ ),
+        array(),
+        '1.0.9',
+        'all'
+    );
+}
+
+add_action( 'wp_enqueue_scripts', 'about_plugin_enqueue_styles' );
